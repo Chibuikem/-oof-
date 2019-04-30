b49049c002304ba7ed91c808a506498eb57f218e
Chibuikem Ajulu-Okeke <cajuluokeke@gmail.com>
2019-04-29 19:06:02 -0700

    Create converter.c

---
+++ Binary and Text Files Converter/converter.c
@@ -0,0 +1,24 @@
+#include "prototypes.h"
+
+int main(int argc, char *argv[]){
+	
+	FILE *ifp;
+	FILE *ofp;
+	long pos;
+	int c, d;
+	
+	if (argc > 4) {
+		fprintf(stderr, "Usage: %s ftest file\n", argv[0]); 
+		exit(1);
+	}
+	
+	if(strcmp(argv[1], "-t") == 0){
+		textToBinary(argv[2], argv[3]);
+	}else if(strcmp(argv[1], "-b") == 0){
+		binaryToText(argv[2], argv[3]);
+	}else if(strcmp(argv[1], "-s") == 0){
+		secondForm(argv[2]);		
+	}
+	
+	return 0;
+}
