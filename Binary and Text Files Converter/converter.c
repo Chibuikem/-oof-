378188a331e14fef423bb2d4c72ad08d6d6c7ee9
Chibuikem Ajulu-Okeke <cajuluokeke@gmail.com>
2019-05-01 17:13:16 -0700

    Update converter.c
    Removed unneeded variables and added default exit statement at the end of if statement

--- Binary and Text Files Converter/converter.c
+++ Binary and Text Files Converter/converter.c
@@ -2,22 +2,21 @@
 
 int main(int argc, char *argv[]){
 	
-	FILE *ifp;
-	FILE *ofp;
-	long pos;
-	int c, d;
-	
 	if (argc > 4) {
 		fprintf(stderr, "Usage: %s ftest file\n", argv[0]); 
 		exit(1);
 	}
 	
+	//checks for the appropriate flag to use appropriate method
 	if(strcmp(argv[1], "-t") == 0){
 		textToBinary(argv[2], argv[3]);
 	}else if(strcmp(argv[1], "-b") == 0){
 		binaryToText(argv[2], argv[3]);
 	}else if(strcmp(argv[1], "-s") == 0){
 		secondForm(argv[2]);		
+	}else{
+		printf("Inappropraite Usage: Check Flag or Number of Arguments\n"); 
+		exit(1);
 	}
 	
 	return 0;
